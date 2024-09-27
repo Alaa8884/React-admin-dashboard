aee3d50abf1023b2456e439bd3f5a54c254c24bc
Alaa Mohammed Youssef <alaamuhamed8@gamil.com>
2024-09-27 20:20:48 +0300

    Build faq page

--- src/pages/faq/FaqPage.jsx
+++ src/pages/faq/FaqPage.jsx
@@ -1,5 +1,133 @@
+import { useState } from "react";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { deepPurple } from "@mui/material/colors";
+
 function FaqPage() {
-  return <div>FaqPage</div>;
+  const theme = useTheme()
+  const [expanded, setExpanded] = useState(false);
+
+  // @ts-ignore
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <Stack direction={"column"} gap={2}>
+      <Accordion
+        // @ts-ignore
+        // expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+        defaultExpanded
+        sx={{
+          background: theme.palette.mode === "light" ? deepPurple[100] : "",
+        }}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            General settings
+          </Typography>
+          <Typography sx={{ color: "text.secondary" }}>
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+            Aliquam eget maximus est, id dignissim quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+     
+      <Accordion
+        // @ts-ignore
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+        sx={{
+          background: theme.palette.mode === "light" ? deepPurple[100] : "",
+        }}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>Users</Typography>
+          <Typography sx={{ color: "text.secondary" }}>
+            You are currently not an owner
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Donec placerat, lectus sed mattis semper, neque lectus feugiat
+            lectus, varius pulvinar diam eros in elit. Pellentesque convallis
+            laoreet laoreet.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        // @ts-ignore
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+        sx={{
+          background: theme.palette.mode === "light" ? deepPurple[100] : "",
+        }}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            Advanced settings
+          </Typography>
+          <Typography sx={{ color: "text.secondary" }}>
+            Filtering has been entirely disabled for whole web server
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
+            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        // @ts-ignore
+        expanded={expanded === "panel4"}
+        onChange={handleChange("panel4")}
+        sx={{
+          background: theme.palette.mode === "light" ? deepPurple[100] : "",
+        }}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            Personal data
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
+            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Stack>
+  );
 }
 
-export default FaqPage
+export default FaqPage;
